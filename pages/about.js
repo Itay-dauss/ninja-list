62e49a4d7cf7ba9bcf465216945171405d04b2c5
itay_daoss <itay.dauss@gmail.com>
2023-02-09 13:34:23 +0200

    initialize

---
+++ pages/about.js
@@ -0,0 +1,23 @@
+import Head from "next/head";
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja | About</title>
+      </Head>
+
+      <div>
+        <h1>About</h1>
+        <p>
+          Quis ex proident ad aliquip qui et culpa nisi et dolor. Velit id amet
+          labore irure non. Amet duis sint occaecat minim consequat minim
+          laborum elit ipsum consequat commodo consequat. Aliquip pariatur nisi
+          culpa exercitation ea ut enim cupidatat.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default About;
